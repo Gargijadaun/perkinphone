3226802064f66ea83e661673de9b41a0725a982d
gargi <131345129+GargiJad@users.noreply.github.com>
2023-12-04 15:51:31 +0530

    df

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-
-
-
 const scriptURL = 'https://script.google.com/macros/s/AKfycby3BJTWfMAXDcLN3JMK-tG8OB_0HVir7QFnrgAIkLabKDgWsFYU_CszNaCcnYRdOWlv/exec'
 
 const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
+
+  // Check if all form fields are filled
+  if (validateForm()) {
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        if (response.ok) {
+          alert("Thank you! Your form is submitted successfully.")
+          window.location.href = './welcome.html'; // Change this URL to the desired next page URL
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      })
+      .catch(error => console.error('Error!', error.message))
+  } else {
+    alert("Please fill in all the fields before submitting the form.")
+  }
 })
+
+// Function to validate the form fields
+function validateForm() {
+  let isValid = true;
+  const formElements = form.elements;
+
+  for (let i = 0; i < formElements.length; i++) {
+    if (formElements[i].type !== 'submit' && formElements[i].value.trim() === '') {
+      isValid = false;
+      break;
+    }
+  }
+
+  return isValid;
+}
