62d1292c288688be6626ead0e92f25186811ba38
gargi <131345129+GargiJad@users.noreply.github.com>
2023-12-04 18:39:22 +0530

    df

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycby3BJTWfMAXDcLN3JMK-tG8OB_0HVir7QFnrgAIkLabKDgWsFYU_CszNaCcnYRdOWlv/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycby3BJTWfMAXDcLN3JMK-tG8OB_0HVir7QFnrgAIkLabKDgWsFYU_CszNaCcnYRdOWlv/exec';
 
-const form = document.forms['contact-form']
+const form = document.forms['contact-form'];
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
   // Check if all form fields are filled
   if (validateForm()) {
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
+    // Get the selected project value from the dropdown
+    const selectedProject = form.elements['project'].value;
+
+    // Create a new FormData object and append the selected project
+    const formData = new FormData(form);
+    formData.append('selectedProject', selectedProject);
+
+    // Make the fetch request
+    fetch(scriptURL, { method: 'POST', body: formData })
+      .then((response) => {
         if (response.ok) {
-          alert("Thank you! Your form is submitted successfully.")
+          alert('Thank you! Your form is submitted successfully.');
           window.location.href = './welcome.html'; // Change this URL to the desired next page URL
         } else {
           throw new Error('Network response was not ok.');
         }
       })
-      .catch(error => console.error('Error!', error.message))
+      .catch((error) => console.error('Error!', error.message));
   } else {
-    alert("Please fill in all the fields before submitting the form.")
+    alert('Please fill in all the fields before submitting the form.');
   }
-})
+});
 
 // Function to validate the form fields
 function validateForm() {
@@ -28,7 +36,10 @@ function validateForm() {
   const formElements = form.elements;
 
   for (let i = 0; i < formElements.length; i++) {
-    if (formElements[i].type !== 'submit' && formElements[i].value.trim() === '') {
+    if (
+      formElements[i].type !== 'submit' &&
+      formElements[i].value.trim() === ''
+    ) {
       isValid = false;
       break;
     }
